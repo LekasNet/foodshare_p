7d7c7ddb5a0baabfbd5bd0e9a368ed8c6227ce0c
LekasNet <lekas1988@gmail.com>
2020-05-03 18:17:19 +0300

    created 3 new pages (order, contact and FAQ) and started to update order system

--- index.py
+++ index.py
@@ -9,5 +9,20 @@ def welcome():
     return render_template("main_page.html")
 
 
+@app.route('/order')
+def order():
+    return render_template("order_page.html")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contact_page.html")
+
+
+@app.route('/FAQ')
+def faq():
+    return render_template("faq_page.html")
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
