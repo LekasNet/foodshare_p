24d391559971d49976897bc6a57b57e7e3624289
LekasNet <lekas1988@gmail.com>
2020-05-04 21:49:11 +0300

    added login page. Updated login style. Added login form parser (form.py)

--- index.py
+++ index.py
@@ -39,8 +39,8 @@ def test():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}'.format(
+            form.username.data))
         return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
 
