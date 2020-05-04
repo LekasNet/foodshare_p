a73cfb20425a08010dc78d5420ea488aa5a0dafa
LekasNet <lekas1988@gmail.com>
2020-05-04 13:05:47 +0300

    beta brench for unwork ideas

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from jinja2 import Template
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 app = Flask(__name__, static_folder="static")
@@ -22,6 +33,14 @@ def contacts():
 @app.route('/FAQ')
 def faq():
     return render_template("faq_page.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
