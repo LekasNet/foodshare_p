402072980324c70c5074150142214d97b0edf064
LekasNet <lekas1988@gmail.com>
2020-05-05 22:53:19 +0300

    modified:   __pycache__/forms.cpython-38.pyc 	modified:   forms.py 	modified:   index.py 	modified:   static/css/login.css 	modified:   static/css/style.css 	modified:   templates/base.html 	new file:   templates/base_card.html 	new file:   templates/card.html 	modified:   templates/order_page.html 	new file:   templates/test_page.html 	new file:   xls_format.py 	created xls-formating system 	added template for product cards 	updated main menu. it is static while scrolling now. 	updated base template 	updated more other styles 	fixed a lot of bugs

--- index.py
+++ index.py
@@ -17,7 +17,9 @@ def welcome():
 
 @app.route('/order')
 def order():
-    return render_template("order_page.html")
+    ask = render_template('card.html')
+    test_page = [ask for _ in range(10)]
+    return render_template("order_page.html").format(' '.join(test_page))
 
 
 @app.route('/contacts')
@@ -32,17 +34,32 @@ def faq():
 
 @app.route('/test')
 def test():
-    return rendered_page
+    return render_template("test_page.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.username.data))
+        print('Login requested for user {} with password {}'.format(
+            form.username.data, form.password.data))
         return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
+
+
+"""
+@app.route('/add_card', methods=['GET', 'POST'])
+def add_card():
+    form = LoginForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        datafilename = secure_filename(f.filename)
+        f.save(os.path.join(
+            app.instance_path, 'photos', filename
+        ))
+        return redirect(url_for('index'))
+    return render_template('upload.html', form=form)
+"""
 
 
 if __name__ == '__main__':
