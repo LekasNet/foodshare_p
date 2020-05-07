ae29a47c86f919842f2db53f4af20f287ef94680
LekasNet <lekas1988@gmail.com>
2020-05-07 23:11:41 +0300

    modified:   index.py 	new file:   ngrok.exe 	new file:   requirements.txt 	modified:   static/css/style.css 	modified:   templates/pages.html 	modified:   templates/template.html 	added online server client, administrator accounts (with secret 	page). added requierements.txt. changed position style on order 	page. started work with maps api in branch beta 017.

--- index.py
+++ index.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from flask import render_template
+from flask_ngrok import run_with_ngrok
 from forms import LoginForm
 import os
 
 
 app = Flask(__name__, static_folder="static")
+run_with_ngrok(app)
+
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+request_name = ''
+logins = ['adm', 'adm2', 'adm3']
+adm = (request_name in logins)
 
 
 @app.route('/')
 def welcome():
-    return render_template("main_page.html", main="current")
+    print(request_name + ' lol ')
+    adm = request_name in logins
+    return render_template("main_page.html", main="current", adm=adm)
 
 
 @app.route('/order')
@@ -23,17 +31,17 @@ def order():
     for i in range(10):
         num = i // (10 // 2) + 1
         test_page.append(ask.format(str(num)))
-    return render_template("order_page.html", order="current").format(' '.join(test_page))
+    return render_template("order_page.html", order="current", adm=adm).format(' '.join(test_page))
 
 
 @app.route('/contacts')
 def contacts():
-    return render_template("contact_page.html")
+    return render_template("contact_page.html", contacts="current", adm=adm)
 
 
 @app.route('/FAQ')
 def faq():
-    return render_template("faq_page.html")
+    return render_template("faq_page.html", faq="current", adm=adm)
 
 
 @app.route('/test')
@@ -43,10 +51,12 @@ def test():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global request_name
     form = LoginForm()
     if form.validate_on_submit():
         print('Login requested for user {} with password {}'.format(
             form.username.data, form.password.data))
+        request_name = form.username.data
         return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
 
@@ -67,4 +77,6 @@ def add_card():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    '''port = int(os.environ.get("PORT", 5000))
+    host='0.0.0.0', port=port'''
+    app.run()
