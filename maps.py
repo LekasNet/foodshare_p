9b86630a0d9a0a09a7800e0703c9d04a494de404
Massimo-stalnoy-strelok-iz-myasa <57673259+Massimo-stalnoy-strelok-iz-myasa@users.noreply.github.com>
2020-05-07 12:51:03 +0300

    Add files via upload

---
+++ maps.py
@@ -0,0 +1,75 @@
+import pygame, requests, sys, os, math
+
+
+class MapParams(object):
+    def __init__(self):
+        self.lat = 55.790707  # Координаты центра карты на старте. Наша школа
+        self.lon = 37.796510
+        self.zoom = 18  # Масштаб карты на старте. Изменяется от 1 до 19
+        self.type = "map"  # Другие значения "sat", "sat,skl"
+
+    # Преобразование координат в параметр ll
+    def ll(self):
+        return str(self.lon) + "," + str(self.lat)
+
+    def update(self, event):
+        my_step = 0.008
+        if event.key == 280 and self.zoom < 19:  # Page_UP
+            self.zoom += 1
+        elif event.key == 281 and self.zoom > 2:  # Page_DOWN
+            self.zoom -= 1
+        elif event.key == 276:  # LEFT_ARROW
+            self.lon -= my_step * math.pow(2, 15 - self.zoom)
+        elif event.key == 275:  # RIGHT_ARROW
+            self.lon += my_step * math.pow(2, 15 - self.zoom)
+        elif event.key == 273 and self.lat < 85:  # UP_ARROW
+            self.lat += my_step * math.pow(2, 15 - self.zoom)
+        elif event.key == 274 and self.lat > -85:  # DOWN_ARROW
+            self.lat -= my_step * math.pow(2, 15 - self.zoom)
+
+
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+    return map_file
+
+
+def main():
+    # Инициализируем pygame
+    pygame.init()
+    pygame.display.set_caption('test caption')
+    screen = pygame.display.set_mode((650, 450))
+    mp = MapParams()
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:  # Выход из программы
+            break
+        elif event.type == pygame.KEYUP:  # Обрабатываем различные нажатые клавиши.
+            mp.update(event)
+
+        # Создаем файл
+        map_file = load_map(mp)
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        karta = pygame.image.load(map_file)
+        scale = pygame.transform.scale(karta, (650, 450))
+        screen.blit(scale, (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    # Удаляем файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
